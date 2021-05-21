3d918eff4ca95fa0f63fb3e8d19fa19930afdd88
Emre Tok <emretok44@hotmail.com>
2021-05-21 17:49:49 +0300

    First123

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        int sayac=0,test=10, total=1;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Sayı: ");
+        int x = in.nextInt();
+
+        // Basamak sayısı
+        for (int i=1; i<=x; i*=10){
+            sayac++;
+        }
+        */
+
+
+    }
+}
